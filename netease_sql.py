7b61bc0d0941fa63e08cf7828271a581e35580a3
Lisen Zhang <245062522@qq.com>
2017-04-14 19:20:30 +0800

    Update netease_sql.py
    Using OOP method to replace the functions: Class Conn

--- netease_sql.py
+++ netease_sql.py
@@ -1,89 +1,89 @@
 #encoding:utf-8
 '''
+@author: Lisen
 Created on 2017年4月12日
-
+--------Changed on 2017年4月14日----------
+Using OOP method to replace the functions
+------------------------------------------
 @author: Lisen
 '''
 import MySQLdb
-import getconfig
-from __builtin__ import str
 
-host = getconfig.host
-user = getconfig.user
-pwd  = getconfig.pwd
-schema = getconfig.schema
-
-db = MySQLdb.connect(host, user, pwd, schema, charset = 'utf8')
-db_cursor = db.cursor()
-
-# execute sql
-def execute_sql(sql_string):
-    global db
-    global db_cursor
-    '''
-     执行SQL语句
-    @param db: object for database connection 
-    @param db_cursor: database cursor for specified schema.table
-    @param sql_string: sql dml string
-    '''    
-    try:
-        db_cursor.execute(sql_string)
-        db.commit()
-    except Exception,e:
-        print "Error in %s" % e
-        db.rollback()
-
-# check exist
-def check_exist(sql_string):
-    global db
-    global db_cursor
-    ''' 
-   查看是否已经在数据库中
-    @param db: object for database connection 
-    @param db_cursor: database cursor for specified schema.table
-    @param sql_string: sql dml string
-    @return: flag to indicate if the record exists in the table
-    '''
-    res_len = len(query_sql(sql_string))
-    if res_len > 0:
-        return True    # 在数据库中
-    else:
-        return False   # 不在数据库中
-    return
-
-# query data
-def query_sql(sql_string):
-    global db
-    global db_cursor
-    '''
-    查询SQL
-    @param db: object for database connection 
-    @param db_cursor: database cursor for specified schema.table
-    @param sql_string: sql dml string
-    @return: sql queries result
-    '''
-    try:
-        db_cursor.execute(sql_string)
-        result = db_cursor.fetchall()
-        return result
-    except Exception, e:
-        print "Error in %s" % e
-
-def close():
-    db.close()
-
-
-def esccape_str(str):
-    return MySQLdb.escape_string(str)
-
-
-
+class Conn(object):
+    """
+   Class for database connection
+    ATTR:  db:      database connection
+           cursor:  cursor for current connection db
+    FUNC:  query_sql
+           exce_sql
+           exist_check
+           db_close
+    INIT:  db_open_connect
+    """
+    def __init__(self, host, user, pwd, schema):
+        """
+        Initialize the database connection when create an object
+        @param host:   database ip 
+        @param user:   database user
+        @param pwd:    database user password
+        @param schema: database specific schema
+        """
+        self.db = MySQLdb.connect(host, user, pwd, schema, charset='utf8')
+        self.cursor = self.db.cursor()
+    
+    # query sql 
+    def query_sql(self, sql):
+        """
+        Using sql string to query specific database table
+        @param self: instance
+        @param sql:  sql string to be executed for querying data  
+        """
+        try:
+            self.cursor.execute(sql)
+        except Exception, e:
+            print "Woops, error %s" % str(e) + "when querying database using " \
+                                                                        + sql
+        else:
+            return self.cursor.fetchall()
+            
+            
+    def exec_sql(self, sql):
+        """
+        Executing insert/update/alter actions using sql string
+        @param self: instance
+        @param sql:  sql string to be executed 
+        """
+        try:
+            self.cursor.execute(sql)
+        except Exception, e:
+            print "Woops, error %s" % str(e) + "when querying database using " \
+                                                                        + sql
+            self.db.rollback()
+        else:
+            self.db.commit()
+    
+    
+    def check_exist(self, sql):
+        """
+        Checking duplication before changing database table's records
+        @param self: instance
+        @param sql:  sql string to be checked 
+        """ 
+        try:
+            check_result = self.query_sql(sql)
+        except Exception, e:
+            print "Woops, error %s" % str(e) + "when querying database using " \
+                                                                        + sql
+        else:
+            if len(check_result):
+                return True
+            else:
+                return False
 
 
-
-
-
-
-
-
-
+    def close(self):
+        """
+        Close database connection
+        @param self: instance 
+        """
+        self.db.close()
